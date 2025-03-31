bfb1e3e337eab45289cbc661569fae945003b611
CoBeatDe <mr.kurshin@mail.ru>
2025-03-31 23:31:13 +0800

    Функция усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if ( str.length > maxlength ) {
+    return str.slice(0, maxlength - 1 ) + '…'
+  }
+  if ( str.length <= maxlength ){
+    return str;
+  }
 }
